89fe363496542386f1d42c88e4a41ec3fc2c5125
Atharva Pimple <atharvapimple30@gmail.com>
2024-11-20 17:09:25 +0530

    Shows selected category

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom'
 export class Navbar extends Component {
     
     render() {
+        let selectedIdx=1;
         return (
             <div>
                 <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
@@ -14,25 +15,25 @@ export class Navbar extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
+                                <li className={selectedIdx===1 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=1}}>
                                     <Link className="nav-link" aria-current="page" to="/">Home</Link>
                                 </li>
-                                <li className="nav-item">
+                                <li className={selectedIdx===2 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=2}}>
                                     <Link className="nav-link" to="/business">business</Link>
                                 </li>
-                                <li className="nav-item">
+                                <li className={selectedIdx===3 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=3}}>
                                     <Link className="nav-link" to="/entertainment">entertainment</Link>
                                 </li>
-                                <li className="nav-item">
+                                <li className={selectedIdx===4 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=4}}>
                                     <Link className="nav-link" to="/health">health</Link>
                                 </li>
-                                <li className="nav-item">
+                                <li className={selectedIdx===5 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=5}}>
                                     <Link className="nav-link" to="/science">science</Link>
                                 </li>
-                                <li className="nav-item">
+                                <li className={selectedIdx===6 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=6}}>
                                     <Link className="nav-link" to="/sports">sports</Link>
                                 </li>
-                                <li className="nav-item">
+                                <li className={selectedIdx===7 ? "nav-item active":"nav-item"} onClick={()=>{selectedIdx=7}}>
                                     <Link className="nav-link" to="/technology">technology</Link>
                                 </li>
                                 
